a506c095f36620c1dbaa0e6c3d02f64dff8373d4
Omkar Barbade <143923167+Omkar9689@users.noreply.github.com>
2024-08-28 10:11:01 +0530

    Update remainderWithoutMod.java

--- Assignments/remainderWithoutMod.java
+++ Assignments/remainderWithoutMod.java
@@ -1,20 +1,31 @@
+/* Java program to find remainder without using mod operator */
+
 import java.util.*;
 
 public class remainderWithoutMod {
 
 	public static void main(String[] args) {
+		
 		int num1,num2;
 		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("Enter First Number");
 		num1=sc.nextInt();
+		
 		System.out.println("Enter Second Number");
 		num2=sc.nextInt();
 		
 		System.out.println("Answer using Modulous Operator");
-		System.out.println(num1+ " % " +num2+ " = "+num1%num2);
+		System.out.println(num1 + " % " + num2 + " = "+ (num1 % num2));				//answer using mod operator
 		
 		System.out.println("Answer without using Modulous Operator");
-		System.out.println(num1-((num1/num2)*num2));
+		System.out.println( num1 - ( ( num1 / num2 )  * num2) );					//answer without using mod operator
+		
+		/* Steps for the logic
+		1)Find quotient by using "/" divide operator i.e quotient = num1/num2
+		2)Subtract the product of quotient and divisor i.e "quotient * num2"
+			from the dividend i.e ans = num1 - (quotient*num2)
+		*/
 	}
 
 }
