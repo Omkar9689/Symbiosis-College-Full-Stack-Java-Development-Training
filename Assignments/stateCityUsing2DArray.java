645c22a5c93a085b8641ae4f9b48b8a0d54c92e9
Omkar Barbade <143923167+Omkar9689@users.noreply.github.com>
2024-08-30 10:10:29 +0530

    Add files via upload

---
+++ Assignments/stateCityUsing2DArray.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class stateCityUsing2DArray {
+
+	public static void main(String[] args) {
+		
+		String str[][]= new String[2][2];
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i = 0; i < 2; i++) {
+			for(int j=0;j< 2;j++) {
+				if(j==0)
+				{
+					System.out.println("Enter State");
+					str[i][j]=sc.nextLine();
+				}
+				else {
+					System.out.println("Enter Name of City Belong to State "+str[i][0]);
+					str[i][j]=sc.nextLine();
+				}
+			}
+		}
+		
+		System.out.println("------------------------");
+		for(int i = 0; i < 2 ; i++) {
+			for(int j=0;j< 2;j++) {
+					System.out.print(str[i][j]+" \t");
+			}
+			System.out.println();
+		}
+	}
+}
