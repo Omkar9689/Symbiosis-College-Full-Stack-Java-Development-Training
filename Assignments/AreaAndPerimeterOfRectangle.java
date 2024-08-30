91e47b90997b188b97b25df1adc4cfbd1f4f5be9
Omkar Santosh Barbade <143923167+Omkar9689@users.noreply.github.com>
2024-08-30 21:08:43 +0530

    Update AreaAndPerimeterOfRectangle.java

--- Assignments/AreaAndPerimeterOfRectangle.java
+++ Assignments/AreaAndPerimeterOfRectangle.java
@@ -6,12 +6,12 @@ public class AreaAndPerimeterOfRectangle {
 	
 	static void perimeter(int length,int breadth){
 		
-		System.out.println("Perimeter of Rectangle with Lenght :- "+length+" and Breadth :- "+breadth+" is "+(2*(length+breadth)));
+		System.out.println("Perimeter of Rectangle with Lenght :- "+length+"cm and Breadth :- "+breadth+"cm is "+(2*(length+breadth))+"cm");
 	}
 	
 	static void area(int length,int breadth) {
 		
-		System.out.println("Area of Rectangle with Lenght :- "+length+" and Breadth :- "+breadth+" is "+(length*breadth));
+		System.out.println("Area of Rectangle with Lenght :- "+length+" and Breadth :- "+breadth+" is "+(length*breadth)+"sq cm");
 	}
 	public static void main(String args[]) {
 		int length,breadth;
