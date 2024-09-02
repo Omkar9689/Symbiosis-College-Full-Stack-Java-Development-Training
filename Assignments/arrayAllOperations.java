b0d6e4ed9423050df5e928f53a133f785b56b244
Omkar Santosh Barbade <143923167+Omkar9689@users.noreply.github.com>
2024-09-02 21:09:45 +0530

    Update and rename sumOfElementsOfArray.java to arrayAllOperations.java

---
+++ Assignments/arrayAllOperations.java
@@ -0,0 +1,80 @@
+/*Java Program to demostrate various array opetations*/
+
+import java.util.Scanner;
+
+public class arrayAllOperations{
+	
+	
+	static int[] sortedArray(int arr[]) {							//sort method (bubble sort)
+		
+		int temp;
+		int arrSize=arr.length;
+		for(int i=0;i<arrSize-1;i++) {
+			for(int j=0;j<arrSize-i-1;j++) {
+			
+				if(arr[j]>arr[j+1]) {
+					temp = arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		}
+		return arr;
+	}
+	
+	public static void main(String[] args) {
+		
+		int arrSize,arraySum=0;
+		float avgOfArrayElements;
+		int[] arr;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter how many elements you want to insert in array");
+		arrSize = sc.nextInt();
+		
+		arr=new int[arrSize];
+		
+		System.out.println("One by one enter "+arrSize+" elments of Array");				
+		
+		for(int i=0;i<arrSize;i++){
+			arr[i]=sc.nextInt();									//storing array elements
+		}
+		
+		
+		System.out.println("\n----------------Array Elements-----------");				
+		for(int i=0;i<arrSize;i++){
+			System.out.print("Element :- "+arr[i]+"\t");						//displaying array
+			//arraySum +=arr[i];  we can use this statement to directly calculate sum of elements without using seperate loop
+		}
+		
+		System.out.println();
+		System.out.println("\n----------------Array Elements in Reverse Order-----------");
+		for(int i=arrSize-1;i>=0;i--){
+			System.out.print("Element :- "+arr[i]+"\t");						//displaying array in reverse order
+		}
+
+		for(int i=0;i<arrSize;i++){
+			arraySum +=arr[i];									//adding array elements
+		}
+		System.out.println("\n\nSum of all Elements of array is "+arraySum);
+		
+		avgOfArrayElements = arraySum / arrSize;							//calculating average of array elements
+		System.out.println("\nAverage of Elements of array is "+avgOfArrayElements);			//dsiplaying average
+		
+		
+		System.out.println("\n----------------Unsorted(Initial) Array-----------");
+		for(int i=0;i<arrSize;i++){
+			System.out.print("Element :- "+arr[i]+"\t");						//displaying initial unsorted array
+		}
+
+		arr=sortedArray(arr);										//calling sort method
+		
+		System.out.println("\n---------------------Sorted Array-------------------");
+		for(int i=0;i<arrSize;i++){
+			System.out.print("Element :- "+arr[i]+"\t");						//displaying sorted array
+		}
+		
+	}
+
+}
