94b522665d2deecc1c14ac1cae5cd8f18752fab2
Omkar Santosh Barbade <143923167+Omkar9689@users.noreply.github.com>
2024-09-10 11:32:43 +0530

    Add files via upload

---
+++ Assignments/HierarchiInheritance/ExcaptionExample.java
@@ -0,0 +1,25 @@
+
+public class ExcaptionExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=100,b=0;
+		try {
+				int c=a/b;
+				
+				System.out.println(" C : "+c);
+		}catch(ArithmeticException e) {
+			System.out.println("Divide by 0 Error");
+			//System.out.println(e.getMessage());
+		}
+		
+		int d=a+b;
+		System.out.println(" D : "+d);
+		
+		int e=a-b;
+		System.out.println(" E : "+e);
+
+	}
+
+}
---
+++ Assignments/HierarchiInheritance/ExceptionExample2.java
@@ -0,0 +1,40 @@
+import javax.naming.PartialResultException;
+
+public class ExceptionExample2 {
+	
+	public static void method1(){};
+	public static void method2() {
+		try {
+		int a=0;
+		int b=10/0;
+		System.out.println("B : "+b);
+		
+		int c[] = {1,2,3,4,5};
+		
+		c[15]=15;
+		
+		System.out.println("C[15] : "+c[1]);
+		}
+		catch(ArithmeticException e1) {
+			System.out.println(e1.getMessage());
+		}
+		catch(ArrayIndexOutOfBoundsException e2) {
+			System.out.println(e2.getMessage());
+		}
+		catch(Exception e3) {
+			System.out.println(e3.getMessage());
+		}
+		finally {
+			method2();
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		method2();
+	
+
+			
+	}
+
+}
