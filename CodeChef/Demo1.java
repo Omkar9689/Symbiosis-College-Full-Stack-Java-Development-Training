674a58f84691a29fff830ebbaa16484524627faf
Omkar Santosh Barbade <143923167+Omkar9689@users.noreply.github.com>
2024-09-26 22:31:42 +0530

    Add files via upload

---
+++ CodeChef/Demo1.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num1,num2;
+		System.out.println("Enter first Number :- ");
+		num1=sc.nextInt();
+		System.out.println("Enter Second Number :- ");
+		num2=sc.nextInt();
+		if(num1>num2) {
+			System.out.println("SUm of "+num1+" & "+num2+" is :- "+(num1+num2));
+			
+		}
+		else {
+			System.out.println("Diference between "+num1+" & "+num2+" is :- "+(num1-num2));
+		}
+	}
+}
