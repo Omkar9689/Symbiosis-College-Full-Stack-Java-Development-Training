1096199ff58ffe8d0a556b62087042cf5b42b5f6
Omkar Santosh Barbade <143923167+Omkar9689@users.noreply.github.com>
2024-09-26 23:03:52 +0530

    Add files via upload

---
+++ CodeChef/Ddemo2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Ddemo2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter String:");
+        String str = sc.nextLine().toLowerCase(); 
+
+        List<Character> vowelList = new ArrayList<>();
+        List<Character> consonantList = new ArrayList<>();
+
+        for (int i = 0; i < str.length(); i++) {
+        	
+            char ch = str.charAt(i);
+            if (Character.isLetter(ch)) { 
+            	
+                if ("aeiou".indexOf(ch) != -1) { 
+                    vowelList.add(ch);
+                }
+                else { 
+                    consonantList.add(ch);
+                }
+            }
+        }
+
+        System.out.println("Num of Vowels:- "+vowelList.size());
+
+        System.out.println("Num of Consonants:- "+consonantList.size());
+        sc.close(); 
+    }
+}
